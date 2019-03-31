34676bb5cd49767efab9a750a2ebefa23530dbc8
O'Toole, James <james.otoole@ubc.ca>
2019-03-31 11:42:52 -0700

    Add ubc_layouts module
    Add module to register and share layouts

---
+++ ubc_layouts/js/ubc.layouts.accordion.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Javascript functionality for Accordions.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+  Drupal.behaviors.ubc_accordion = {
+    attach: function (context) {
+
+    }
+  };
+})(jQuery, Drupal);
