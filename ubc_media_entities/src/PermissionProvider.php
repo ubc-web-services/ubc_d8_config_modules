21b6d722420a630b25dafea715ce5eb157c7fd1f
James O'Toole <james.otoole@ubc.ca>
2025-05-26 11:02:16 -0700

    Update to clean up coding standards issues

--- ubc_media_entities/src/PermissionProvider.php
+++ ubc_media_entities/src/PermissionProvider.php
@@ -8,34 +8,33 @@ use Drupal\media\Entity\MediaType;
 /**
  * Provides dynamic permissions for media types.
  */
-class PermissionProvider
-{
-    use StringTranslationTrait;
+class PermissionProvider {
+  use StringTranslationTrait;
 
-    /**
-     * Generates dynamic permissions for all media types.
-     *
-     * @return array
-     *   An array of permissions.
-     */
-    public static function generateMediaPermissions()
-    {
-        $permissions = [];
-        $media_types = MediaType::loadMultiple();
+  /**
+   * Generates dynamic permissions for all media types.
+   *
+   * @return array
+   *   An array of permissions.
+   */
+  public static function generateMediaPermissions() {
+    $permissions = [];
+    $media_types = MediaType::loadMultiple();
 
-        foreach ($media_types as $media_type) {
-            $type_id = $media_type->id();
-            $type_label = $media_type->label();
+    foreach ($media_types as $media_type) {
+      $type_id = $media_type->id();
+      $type_label = $media_type->label();
 
-            // Add 'view' permission for the media type.
-            $permissions["view $type_id media"] = [
-            'title' => t('View @type media', ['@type' => $type_label]),
-            'description' => t('Allows users to view media of type @type.', ['@type' => $type_label]),
-            'restrict access' => true,
-            ];
+      // Add 'view' permission for the media type.
+      $permissions["view $type_id media"] = [
+        'title' => t('View @type media', ['@type' => $type_label]),
+        'description' => t('Allows users to view media of type @type.', ['@type' => $type_label]),
+        'restrict access' => TRUE,
+      ];
 
-        }
-
-        return $permissions;
     }
+
+    return $permissions;
+  }
+
 }
